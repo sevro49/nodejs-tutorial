8a3418034eb464b977b4ce4306130cba3e821b34
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-11-20 01:42:02 +0300

    feat: added routes

--- server.js
+++ server.js
@@ -33,44 +33,12 @@ app.use(express.json());
 
 // serve static files
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-  // res.sendFile('./views/index.html', { root: __dirname});
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, '/new-page.html'); //302 by default
-});
-
-// Route Handlers
-app.get('/hello(.html)?', (req, res, next) => {
-  console.log('Attempted to load hello.html');
-  next(); // Pass control to the next handler
-}, (req, res) => {
-  res.send("Hello World!");
-});
-
-// Chaining route handlers
-const one = (req, res, next) => {
-  console.log('One');
-  next();
-}
-
-const two = (req, res, next) => {
-  console.log('Two');
-  next();
-}
-const three = (req, res) => {
-  console.log('three');
-  res.send('Finished!')
-}
-
-app.get('/chain(.html)?', [one, two, three]);
+// routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees')); //doesnt need any static file, just json
 
 // app.all = all http methods
 app.all('*', (req, res) => {
