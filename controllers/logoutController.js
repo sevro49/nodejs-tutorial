7ef2059c38bf746b1ed2c529f5c56c3c010aaaeb
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-11-22 16:58:59 +0300

    feat: authorized roles

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -6,30 +6,41 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 const handleLogout = async (req, res) => {
-  // On client, also delete the accessToken
-
   const cookies = req.cookies;
-  if(!cookies?.jwt) res.sendStatus(204); // No content
+
+  // Eğer JWT yoksa
+  if (!cookies?.jwt) {
+    return res.sendStatus(204); // Yanıt gönder ve işlevi sonlandır
+  }
+
   const refreshToken = cookies.jwt;
 
-  // Is refreshToken in the db?
+  // Veritabanında refreshToken var mı?
   const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
-  if(!foundUser) {
+  if (!foundUser) {
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-    return res.sendStatus(204); // No content
-  }  
-  
-  // Delete refreshToken in db
+    return res.sendStatus(204); // Yanıt gönder ve işlevi sonlandır
+  }
+
+  // RefreshToken'ı sil
   const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
   const currentUser = { ...foundUser, refreshToken: '' };
   usersDB.setUsers([...otherUsers, currentUser]);
-  await fsPromises.writeFile(
-    path.join(__dirname, '..', 'model', 'users.json'),
-    JSON.stringify(usersDB.users)
-  );
 
+  try {
+    await fsPromises.writeFile(
+      path.join(__dirname, '..', 'model', 'users.json'),
+      JSON.stringify(usersDB.users)
+    );
+  } catch (error) {
+    console.error('Error writing to file:', error);
+    return res.sendStatus(500); // Sunucu hatası
+  }
+
+  // Çerezi temizle ve yanıt gönder
   res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
   res.sendStatus(204);
-}
+};
+
 
 module.exports = { handleLogout };
